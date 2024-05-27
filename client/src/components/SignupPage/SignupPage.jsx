69371befd16fec54368fe60c8865ed96c9734a90
Jackie Leary <jacquelinebail788@gmail.com>
2024-05-27 15:43:01 -0400

    complete login and signup components, install react-router-dom, add theme.js, use mui + css modules

---
+++ client/src/components/SignupPage/SignupPage.jsx
@@ -0,0 +1,124 @@
+import styles from './SignupPage.module.css';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+const SignupPage = () => {
+	return (
+		<div
+			style={{
+				position: 'fixed',
+				top: 0,
+				right: 0,
+				bottom: 0,
+				left: 0,
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				background: 'linear-gradient(to bottom, #FFEFD5, #f0f0f0)',
+			}}>
+			<Paper
+				elevation={3}
+				className={styles.paper}>
+				<Typography
+					variant="h2"
+					component="h2"
+					align="center"
+					gutterBottom>
+					shelf<span style={{ fontWeight: 'bold' }}>share</span>
+				</Typography>
+				<Typography
+					variant="body1"
+					component="p"
+					align="center"
+					gutterBottom>
+					CREATE ACCOUNT
+				</Typography>
+				<form className={styles.form}>
+					<Grid
+						container
+						spacing={2}>
+						<Grid
+							item
+							xs={12}>
+							<TextField
+								variant="outlined"
+								label="Your Name"
+								fullWidth
+								autoFocus
+								sx={{ width: '50%' }}
+							/>
+						</Grid>
+						<Grid
+							item
+							xs={12}>
+							<TextField
+								variant="outlined"
+								label="Email"
+								fullWidth
+								autoFocus
+								sx={{ width: '50%' }}
+							/>
+						</Grid>
+						<Grid
+							item
+							xs={12}>
+							<TextField
+								variant="outlined"
+								label="Password"
+								type="password"
+								fullWidth
+								sx={{ width: '50%' }}
+							/>
+						</Grid>
+						<Grid
+							item
+							xs={12}>
+							<TextField
+								variant="outlined"
+								label="Re-enter Password"
+								type="password"
+								fullWidth
+								sx={{ width: '50%' }}
+							/>
+						</Grid>
+						<Grid
+							item
+							xs={12}>
+							<Button
+								variant="contained"
+								color="primary"
+								sx={{ width: '50%' }}
+								component={Link}
+								to="/" // TO-DO: Update to Landing Page
+							>
+								Create Account
+							</Button>
+						</Grid>
+						<Grid
+							item
+							xs={12}>
+							<Typography
+								variant="body1"
+								component="p"
+								align="center"
+								gutterBottom>
+								Already have an account?{' '}
+								<Link
+									to="/"
+									style={{ fontWeight: 'bold' }}>
+									Sign In
+								</Link>
+							</Typography>
+						</Grid>
+					</Grid>
+				</form>
+			</Paper>
+		</div>
+	);
+};
+
+export default SignupPage;
