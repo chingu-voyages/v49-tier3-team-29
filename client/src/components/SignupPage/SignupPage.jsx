01e234c5d20b2325cc7692b510f014eca807a90d
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-28 21:27:15 -0400

    feat: Add user creation state and input

--- client/src/components/SignupPage/SignupPage.jsx
+++ client/src/components/SignupPage/SignupPage.jsx
@@ -5,8 +5,32 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { registerUser } from '../../reducers/userSlice';
 
 const SignupPage = () => {
+	const [name, setName] = useState('');
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [passwordVerifier, setPasswordVerifier] = useState('');
+	const dispatch = useDispatch();
+
+	const handleSubmission = async () => {
+		if (password !== passwordVerifier) {
+			alert('Passwords do not match');
+		}
+		const userDetails = {
+			name,
+			email,
+			password,
+			username,
+		};
+		const user = await dispatch(registerUser(userDetails));
+
+		console.log(user);
+	};
 	return (
 		<div
 			style={{
@@ -46,9 +70,24 @@ const SignupPage = () => {
 							xs={12}>
 							<TextField
 								variant="outlined"
+								label="Username"
+								fullWidth
+								autoFocus
+								value={username}
+								onChange={e => setUsername(e.target.value)}
+								sx={{ width: '50%' }}
+							/>
+						</Grid>
+						<Grid
+							item
+							xs={12}>
+							<TextField
+								variant="outlined"
 								label="Your Name"
 								fullWidth
 								autoFocus
+								value={name}
+								onChange={e => setName(e.target.value)}
 								sx={{ width: '50%' }}
 							/>
 						</Grid>
@@ -60,6 +99,8 @@ const SignupPage = () => {
 								label="Email"
 								fullWidth
 								autoFocus
+								value={email}
+								onChange={e => setEmail(e.target.value)}
 								sx={{ width: '50%' }}
 							/>
 						</Grid>
@@ -71,6 +112,8 @@ const SignupPage = () => {
 								label="Password"
 								type="password"
 								fullWidth
+								value={password}
+								onChange={e => setPassword(e.target.value)}
 								sx={{ width: '50%' }}
 							/>
 						</Grid>
@@ -82,6 +125,10 @@ const SignupPage = () => {
 								label="Re-enter Password"
 								type="password"
 								fullWidth
+								value={passwordVerifier}
+								onChange={e =>
+									setPasswordVerifier(e.target.value)
+								}
 								sx={{ width: '50%' }}
 							/>
 						</Grid>
@@ -93,6 +140,7 @@ const SignupPage = () => {
 								color="primary"
 								sx={{ width: '50%' }}
 								component={Link}
+								onClick={handleSubmission}
 								to="/" // TO-DO: Update to Landing Page
 							>
 								Create Account
