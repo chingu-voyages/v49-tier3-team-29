369af85faf2b4df621138302078f86da892c4c6f
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-31 10:30:31 -0400

    feat: Finalize features section of the landing page

--- client/src/components/LandingPage/Features.jsx
+++ client/src/components/LandingPage/Features.jsx
@@ -1,13 +1,16 @@
-import EdgesensorHighRoundedIcon from '@mui/icons-material/EdgesensorHighRounded';
-import DevicesRoundedIcon from '@mui/icons-material/DevicesRounded';
-import ViewQuiltRounded from '@mui/icons-material/ViewQuiltRounded';
 import Chip from '@mui/material/Chip';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import { useState } from 'react';
-
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import { Link } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import RateReviewIcon from '@mui/icons-material/RateReview';
+import ViewListIcon from '@mui/icons-material/ViewList';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 const Features = () => {
 	const [selectedItemIndex, setSelectedItemIndex] = useState(0);
 
@@ -15,28 +18,28 @@ const Features = () => {
 		setSelectedItemIndex(index);
 	};
 
-	const items = [
+	const features = [
 		{
-			icon: <ViewQuiltRounded />,
+			icon: <RateReviewIcon />,
 			title: 'Book Reviews',
 			description:
-				'This item could provide a snapshot of the most important metrics or data points related to the product.',
+				'Share your thoughts and help others find their next great read with our in-depth book review feature.',
 			imageLight:
 				'url("/static/images/templates/templates-images/dash-light.png")',
 		},
 		{
-			icon: <EdgesensorHighRoundedIcon />,
+			icon: <ViewListIcon />,
 			title: 'Personalized Lists',
 			description:
-				'This item could provide information about the mobile app version of the product.',
+				"Keep your reading life organized - create custom lists to track what you've read, what you're reading, and what you want to read next.",
 			imageLight:
 				'url("/static/images/templates/templates-images/mobile-light.png")',
 		},
 		{
-			icon: <DevicesRoundedIcon />,
+			icon: <LibraryBooksIcon />,
 			title: 'Discovery',
 			description:
-				'This item could let users know the product is available on all platforms, such as web, mobile, and desktop.',
+				'Uncover your next great read by exploring books others have loved and reviewed.',
 			imageLight:
 				'url("/static/images/templates/templates-images/devices-light.png")',
 		},
@@ -45,48 +48,174 @@ const Features = () => {
 		<Box mt={6}>
 			<Grid
 				container
-				spacing={3}>
-				<Grid item>
-					<Typography
-						variant="h5"
-						textAlign={'center'}
-						fontWeight={500}>
-						Shelfshare features
-					</Typography>
-					<Typography variant="body1">
-						Here you can provide a brief overview of the key
-						features of the product. For example, you could list the
-						number of features, the types of features, add-ons, or
-						the benefits of the features.
-					</Typography>
+				spacing={4}>
+				<Grid
+					item
+					container
+					spacing={4}
+					xs={12}
+					md={6}>
+					<Grid
+						item
+						textAlign={{ xs: 'center', sm: 'left' }}>
+						<Typography
+							variant="h5"
+							fontWeight={500}>
+							Shelfshare features
+						</Typography>
+						<Typography variant="body1">
+							Shelfshare is a social network for book lovers where
+							you can track your reading progress, discover new
+							books through recommendations and reviews, and
+							connect with other readers who share your interests.
+						</Typography>
+					</Grid>
+					<Grid
+						container
+						item
+						gap={1}
+						sx={{ display: { xs: 'auto', sm: 'none' } }}>
+						{features.map(({ title }, index) => (
+							<Chip
+								key={index}
+								label={title}
+								onClick={() => handleItemClick(index)}></Chip>
+						))}
+					</Grid>
+					<Grid item>
+						<Card
+							variant="outlined"
+							sx={{ display: { sm: 'none' }, p: 2 }}>
+							<Box>Image</Box>
+							<Box textAlign={'left'}>
+								<Typography
+									variant="body2"
+									fontWeight={'bold'}>
+									{features[selectedItemIndex].title}
+								</Typography>
+								<Typography variant="body2">
+									{features[selectedItemIndex].description}
+								</Typography>
+							</Box>
+						</Card>
+					</Grid>
+					<Grid item>
+						<Stack
+							direction="column"
+							justifyContent="center"
+							spacing={2}
+							useFlexGap
+							sx={{
+								width: '100%',
+								display: { xs: 'none', sm: 'flex' },
+							}}
+							alignItems="flex-start">
+							{features.map(
+								({ icon, title, description }, index) => (
+									<Card
+										key={index}
+										variant="outlined"
+										component={Button}
+										onClick={() => handleItemClick(index)}
+										sx={{
+											p: 3,
+											height: 'fit-content',
+											width: '100%',
+											background: 'none',
+											backgroundColor:
+												selectedItemIndex === index
+													? 'action.selected'
+													: undefined,
+											borderColor:
+												selectedItemIndex === index
+													? 'primary.light'
+													: 'grey.300',
+										}}>
+										<Box
+											sx={{
+												width: '100%',
+												display: 'flex',
+												textAlign: 'left',
+												flexDirection: {
+													xs: 'column',
+													md: 'row',
+												},
+												alignItems: { md: 'center' },
+												gap: 2.5,
+											}}>
+											<Box
+												sx={{
+													color:
+														selectedItemIndex ===
+														index
+															? 'primary.main'
+															: 'grey.300',
+												}}>
+												{icon}
+											</Box>
+											<Box sx={{ textTransform: 'none' }}>
+												<Typography
+													variant="body2"
+													fontWeight="bold"
+													color={'text.primary'}>
+													{title}
+												</Typography>
+												<Typography
+													color={'text.secondary'}
+													variant="body2"
+													sx={{ my: 0.5 }}>
+													{description}
+												</Typography>
+												<Link
+													color={'primary'}
+													variant="body2"
+													fontWeight={'bold'}
+													sx={{
+														display: 'inline-flex',
+														alignfeatures: 'center',
+													}}
+													onClick={event =>
+														event.stopPropagation()
+													}>
+													<span>Learn More</span>
+													<ChevronRightRoundedIcon
+														fontSize="small"
+														sx={{
+															mt: '1px',
+															ml: '2px',
+														}}></ChevronRightRoundedIcon>
+												</Link>
+											</Box>
+										</Box>
+									</Card>
+								)
+							)}
+						</Stack>
+					</Grid>
 				</Grid>
 				<Grid
-					container
 					item
-					gap={1}
-					sx={{ display: { xs: 'auto', sm: 'none' } }}>
-					{items.map(({ title }, index) => (
-						<Chip
-							key={index}
-							label={title}
-							onClick={() => handleItemClick(index)}></Chip>
-					))}
-				</Grid>
-				<Grid item>
+					xs={12}
+					md={6}
+					sx={{
+						display: { xs: 'none', sm: 'flex', width: '100%' },
+					}}>
 					<Card
 						variant="outlined"
-						sx={{ display: { sm: 'none' }, p: 2 }}>
-						<Box>Image</Box>
-						<Box textAlign={'left'}>
-							<Typography
-								variant="body2"
-								fontWeight={'bold'}>
-								{items[selectedItemIndex].title}
-							</Typography>
-							<Typography variant="body2">
-								{items[selectedItemIndex].description}
-							</Typography>
-						</Box>
+						sx={{
+							height: '100%',
+							width: '100%',
+							display: { xs: 'none', sm: 'flex' },
+							pointerEvents: 'none',
+						}}>
+						<Box
+							sx={{
+								m: 'auto',
+								width: 420,
+								height: 500,
+								backgroundSize: 'contain',
+								backgroundImage: 'none', // needs to be changed to screenshot of features!
+							}}></Box>
 					</Card>
 				</Grid>
 			</Grid>
