94b91084e7ea7351ab9b3c1e9fa090e0da5b9ef1
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-06-03 10:27:28 -0400

    feat: Move Navbar and Footer outside LandingPage to be reusued on other pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 // import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Outlet,
+} from 'react-router-dom';
 import './App.css';
 import LoginPage from './components/LoginPage/LoginPage';
 import SignupPage from './components/SignupPage/SignupPage';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme.js';
 import LandingPage from './components/LandingPage/LandingPage.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
 	// const [count, setCount] = useState(0);
@@ -16,9 +23,18 @@ function App() {
 				<Router>
 					<Routes>
 						<Route
-							path="/"
-							element={<LandingPage />}
-						/>
+							element={
+								<>
+									<Navbar></Navbar>
+									<Outlet></Outlet>
+									<Footer></Footer>
+								</>
+							}>
+							<Route
+								path="/"
+								element={<LandingPage />}
+							/>
+						</Route>
 						<Route
 							path="/login"
 							element={<LoginPage></LoginPage>}></Route>
