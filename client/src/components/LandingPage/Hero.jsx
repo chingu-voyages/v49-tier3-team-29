06f735872db715d42121d040052b7df3305a9755
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-06-05 09:45:28 -0400

    fix: Fix sizing issues for books on different devices

--- client/src/components/LandingPage/Hero.jsx
+++ client/src/components/LandingPage/Hero.jsx
@@ -18,22 +18,75 @@ const Hero = () => {
 	}, [dispatch]);
 
 	const books = useSelector(state => state.book.bookInfo);
+
 	const bookImages =
 		books.length > 0 ? (
-			books.map(book => (
-				<Paper
-					key={book.ISBN}
-					elevation={5}
-					sx={{
-						backgroundImage: `url(${book.imageUrl})`,
-						backgroundSize: 'contain',
-						width: { xs: '68px', sm: '200px' },
-						height: { xs: '125px', sm: '300px' },
-					}}></Paper>
-			))
-		) : (
-			<></>
-		);
+			<>
+				<Grid
+					item
+					key={books[0].ISBN}>
+					<Paper
+						elevation={5}
+						sx={{
+							backgroundImage: `url(${books[0].imageUrl})`,
+							backgroundSize: 'contain',
+							width: { xs: '68px', sm: '200px' },
+							height: { xs: '125px', sm: '300px' },
+						}}></Paper>
+				</Grid>
+				<Grid
+					item
+					key={books[1].ISBN}>
+					<Paper
+						elevation={5}
+						sx={{
+							backgroundImage: `url(${books[1].imageUrl})`,
+							backgroundSize: 'contain',
+							width: { xs: '68px', sm: '200px' },
+							height: { xs: '125px', sm: '300px' },
+						}}></Paper>
+				</Grid>
+				<Grid
+					item
+					key={books[2].ISBN}
+					display={{ xs: 'none', md: 'block' }}>
+					<Paper
+						elevation={5}
+						sx={{
+							backgroundImage: `url(${books[2].imageUrl})`,
+							backgroundSize: 'contain',
+							width: { xs: '68px', sm: '200px' },
+							height: { xs: '125px', sm: '300px' },
+						}}></Paper>
+				</Grid>
+				<Grid
+					item
+					key={books[3].ISBN}
+					display={{ xs: 'none', lg: 'block' }}>
+					<Paper
+						elevation={5}
+						sx={{
+							backgroundImage: `url(${books[3].imageUrl})`,
+							backgroundSize: 'contain',
+							width: { xs: '68px', sm: '200px' },
+							height: { xs: '125px', sm: '300px' },
+						}}></Paper>
+				</Grid>
+				<Grid
+					item
+					key={books[4].ISBN}
+					display={{ xs: 'none', lg: 'block' }}>
+					<Paper
+						elevation={5}
+						sx={{
+							backgroundImage: `url(${books[4].imageUrl})`,
+							backgroundSize: 'contain',
+							width: { xs: '68px', sm: '200px' },
+							height: { xs: '125px', sm: '300px' },
+						}}></Paper>
+				</Grid>
+			</>
+		) : null;
 
 	return (
 		<Box>
@@ -51,8 +104,10 @@ const Hero = () => {
 			</Box>
 			<Grid
 				mt={3}
+				spacing={2}
+				overflow={'hidden'}
 				container
-				justifyContent={'space-between'}>
+				justifyContent={'center'}>
 				{bookImages}
 			</Grid>
 		</Box>
