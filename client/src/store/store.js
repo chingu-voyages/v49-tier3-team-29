81d56742cec2db26e3a5ff091a4b9a98feb03872
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-06-05 09:06:11 -0400

    feat: Import review reducer to store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../reducers/userSlice';
 import bookReducer from '../reducers/bookSlice';
+import reviewReducer from '../reducers/reviewSlice';
 
 export const store = configureStore({
 	reducer: {
 		session: userReducer,
 		book: bookReducer,
+		review: reviewReducer,
 	},
 });
