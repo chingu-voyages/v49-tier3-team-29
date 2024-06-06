0394f90c2ea4d56a83ab23aa3c246ea4878f6d48
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-06-06 11:36:52 -0400

    feat: Create ReviewModal component for creating book reviews

---
+++ client/src/components/ReviewModal/ReviewModal.jsx
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import {
+	Card,
+	CardContent,
+	Paper,
+	Rating,
+	Stack,
+	TextField,
+} from '@mui/material';
+import theme from '../../theme';
+
+const ReviewModal = () => {
+	const [open, setOpen] = React.useState(false);
+	const [value, setValue] = React.useState(2);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
+	return (
+		<Box>
+			<Button
+				onClick={handleOpen}
+				variant="contained">
+				Review Book
+			</Button>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description">
+				<Card
+					sx={{
+						width: { xs: '90%', md: '50%' },
+						overflow: 'auto',
+						position: 'absolute',
+						top: '50%',
+						left: '50%',
+						transform: 'translate(-50%,-50%)',
+					}}>
+					<CardContent
+						sx={{
+							display: 'flex',
+							gap: '12px',
+							flexDirection: 'column',
+						}}>
+						<Box
+							display={'flex'}
+							flexDirection={'row'}
+							gap="12px">
+							<Paper
+								elevation={5}
+								sx={{
+									bgcolor: theme.palette.primary.main,
+									width: { xs: '50px' },
+									height: { xs: '100px' },
+								}}></Paper>
+							<Stack textAlign={'left'}>
+								<Typography variant="h4">Book Title</Typography>
+								<Typography
+									variant="h8"
+									color={'text.secondary'}>
+									by Book Author
+								</Typography>
+							</Stack>
+						</Box>
+						<Typography
+							component="legend"
+							alignSelf={'center'}>
+							Your rating:
+						</Typography>
+						<Rating
+							sx={{ alignSelf: 'center' }}
+							name="simple-controlled"
+							value={value}
+							defaultValue={3}
+							size="large"
+							onChange={(event, newValue) => {
+								setValue(newValue);
+							}}
+						/>
+						<TextField placeholder="Write a title for the review"></TextField>
+						<TextField
+							id="outlined-multiline-static"
+							multiline
+							rows={8}
+							placeholder="Write a review"></TextField>
+						<Button
+							variant="contained"
+							size="small"
+							sx={{ width: '20%', alignSelf: 'center' }}>
+							Post
+						</Button>
+					</CardContent>
+				</Card>
+			</Modal>
+		</Box>
+	);
+};
+
+export default ReviewModal;
