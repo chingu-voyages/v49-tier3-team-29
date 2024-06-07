e1a70fb507c9aed8b63b74faf6f20803e0660f85
bank1e <cub0sextra@gmail.com>
2024-06-06 18:11:03 -0700

    Feat/logout-frontend - Added a new function "logoutUser" in authController.js (backend) - Added logout GET route in authRoute.js (backend) - Added authBaseURL in baseUrl.js (frontend) - Added logoutUser Thunk in userSlice.js to initiate get request to log   out user (frontend) - Imported logoutUser from userSlice in Navbar.jsx (frontend) - Imported useDispatch from react-redux in Navbar.jsx (frontend) - Added handleLogout function in Navbar.jsx to alert user with message   and dispatch the action to logoutUser reducer. (frontend)

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { forgotPassword, resetPassword } from "../controller/authController.js";
+import {
+  forgotPassword,
+  resetPassword,
+  logoutUser,
+} from "../controller/authController.js";
 const router = express.Router();
 
 //* initiate password reset
 router.post("/forgot-password", forgotPassword);
 //* process resetting password
 router.post("/reset-password", resetPassword);
+
+//* log user out
+router.get("/logout", logoutUser);
 
 export default router;
