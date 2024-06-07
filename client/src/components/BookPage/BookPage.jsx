20e0b465add11dc51427d1a40bd302676d7df7ab
Jackie Leary <jacquelinebail788@gmail.com>
2024-06-06 23:11:19 -0400

    fetch book

--- client/src/components/BookPage/BookPage.jsx
+++ client/src/components/BookPage/BookPage.jsx
@@ -6,64 +6,67 @@ const BookPage = () => {
 	const { id } = useParams();
 	const [book, setBook] = useState(null);
 
+	const fetchBook = async id => {
+		try {
+			const response = await fetch(`api/book/${id}`);
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.error('Error fetching book data:', error);
+		}
+	};
+
 	useEffect(() => {
-		const fetchBook = async id => {
-			try {
-				const response = await fetch(`/book/${id}`);
-				const data = await response.json();
-				setBook(data);
-			} catch (error) {
-				console.error('Error fetching book data:', error);
-			}
-		};
-
-		fetchBook(id);
-	}, [id]);
-
-	if (!book) {
-		return <div>Loading...</div>;
-	}
+		fetchBook(id).then(data => {
+			setBook(data);
+		});
+	}, []);
 
 	return (
-		<Card>
-			<CardMedia
-				style={{ height: 400 }}
-				image={book.imageUrl}
-				title={book.title}
-			/>
-			<CardContent>
-				<Typography
-					gutterBottom
-					variant="h5"
-					component="h2">
-					{book.title}
-				</Typography>
-				<Typography
-					variant="body1"
-					color="textSecondary"
-					component="p">
-					Author: {book.author}
-				</Typography>
-				<Typography
-					variant="body1"
-					color="textSecondary"
-					component="p">
-					Description: {book.description}
-				</Typography>
-				<Typography
-					variant="body1"
-					color="textSecondary"
-					component="p">
-					ISBN: {book.ISBN}
-				</Typography>
-				<Typography
-					variant="body1"
-					color="textSecondary"
-					component="p">
-					Reviews:
-				</Typography>
-			</CardContent>
-		</Card>
+		<div>
+			{book && (
+				<Card>
+					<CardMedia
+						style={{ height: 400 }}
+						image={book.imageUrl}
+						title={book.title}
+					/>
+					<CardContent>
+						<Typography
+							gutterBottom
+							variant="h5"
+							component="h2">
+							{book.title}
+						</Typography>
+						<Typography
+							variant="body1"
+							color="textSecondary"
+							component="p">
+							Author: {book.author}
+						</Typography>
+						<Typography
+							variant="body1"
+							color="textSecondary"
+							component="p">
+							Description: {book.description}
+						</Typography>
+						<Typography
+							variant="body1"
+							color="textSecondary"
+							component="p">
+							ISBN: {book.ISBN}
+						</Typography>
+						<Typography
+							variant="body1"
+							color="textSecondary"
+							component="p">
+							Reviews:
+						</Typography>
+					</CardContent>
+				</Card>
+			)}
+			,
+		</div>
 	);
 };
 
