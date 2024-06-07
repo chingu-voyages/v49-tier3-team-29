e5acc71230b943b507cce98c6cbd97f6d0c6280e
bank1e <cub0sextra@gmail.com>
2024-06-07 11:51:06 -0700

    Resolve conflict in listRoute.js

--- backend/routes/listRoute.js
+++ backend/routes/listRoute.js
@@ -5,11 +5,11 @@ import {
   getListByUserId,
 } from "../controller/listController.js";
 
-// import { addBookToList } from "../controller/listController.js";
+import { addBookToList } from "../controller/listController.js";
 
 const router = express.Router();
 
-// router.patch("/:listId", addBookToList);
+router.patch("/:listId", addBookToList);
 
 // Get list by user ID
 router.get("/user/:userid", getListByUserId);
