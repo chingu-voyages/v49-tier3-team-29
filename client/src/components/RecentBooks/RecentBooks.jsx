f1d52b369ed9573afa9ea743be3eeb6c9b992528
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-06-11 12:40:47 -0400

    feat: Update component to display books in users list

--- client/src/components/RecentBooks/RecentBooks.jsx
+++ client/src/components/RecentBooks/RecentBooks.jsx
@@ -1,13 +1,82 @@
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import theme from '../../theme';
 import Stack from '@mui/material/Stack';
 import MaterialLink from '@mui/material/Link';
 import { Link } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchList } from '../../reducers/listSlice';
 
 const RecentBooks = () => {
+	const dispatch = useDispatch();
+	const user = useSelector(state => state.session.userInfo);
+
+	// Select the books list from the listInfo
+	const list = useSelector(state => state.list.listInfo.books);
+	useEffect(() => {
+		if (user.username) {
+			dispatch(fetchList(user.username));
+		}
+	}, [user, dispatch]);
+	let recentBooks = (
+		<Typography variant="h6">No books recently added</Typography>
+	);
+	if (list) {
+		if (list.length > 0) {
+			recentBooks = list.map(book => (
+				<Card
+					key={book.ISBN}
+					sx={{ maxWidth: '400px' }}>
+					<CardContent
+						sx={{
+							display: { xs: 'flex' },
+							flexDirection: 'column',
+							gap: 3,
+						}}>
+						<Paper
+							elevation={5}
+							sx={{
+								alignSelf: 'center',
+								backgroundImage: `url(${book.imageUrl})`,
+								backgroundSize: 'cover',
+								backgroundRepeat: 'no-repeat',
+								width: '100px',
+								height: '150px',
+							}}></Paper>
+
+						<Stack textAlign={'left'}>
+							<MaterialLink
+								component={Link}
+								to={'/'}>
+								{book.title}
+							</MaterialLink>
+							<Typography
+								variant="body2"
+								color={'text.secondary'}>
+								by {book.author}
+							</Typography>
+							<Typography
+								variant="body2"
+								mt={2}
+								textOverflow={'ellipsis'}
+								overflow={'hidden'}
+								sx={{
+									lineClamp: 2,
+									display: '-webkit-box',
+									WebkitLineClamp: 3,
+									WebkitBoxOrient: 'vertical',
+								}}>
+								{book.description}
+							</Typography>
+						</Stack>
+					</CardContent>
+				</Card>
+			));
+		}
+	}
+
 	return (
 		<Stack
 			pt={{ xs: 4, sm: 12 }}
@@ -19,82 +88,11 @@ const RecentBooks = () => {
 			<Stack
 				width={{ sx: '100%', sm: '75%', md: '100%' }}
 				justifyContent={'space-around'}
+				alignItems={'center'}
 				direction={{ md: 'row' }}
 				gap={3}>
-				<Card>
-					<CardContent sx={{ display: 'flex', gap: '12px' }}>
-						<Paper
-							elevation={5}
-							sx={{
-								bgcolor: theme.palette.primary.main,
-								width: { xs: '50px' },
-								height: { xs: '75px' },
-							}}></Paper>
-						<Stack textAlign={'left'}>
-							<Typography variant="body1">Book Title</Typography>
-							<Typography
-								variant="body2"
-								color={'text.secondary'}>
-								by Book Author
-							</Typography>
-							<Typography
-								variant="body2"
-								mt={2}>
-								This is a demo book for testing purposes
-							</Typography>
-						</Stack>
-					</CardContent>
-				</Card>
-				<Card>
-					<CardContent sx={{ display: 'flex', gap: '10px' }}>
-						<Paper
-							elevation={5}
-							sx={{
-								bgcolor: theme.palette.primary.main,
-								width: { xs: '50px' },
-								height: { xs: '75px' },
-							}}></Paper>
-						<Stack textAlign={'left'}>
-							<Typography variant="body1">Book Title</Typography>
-							<Typography
-								variant="body2"
-								color={'text.secondary'}>
-								by Book Author
-							</Typography>
-							<Typography
-								variant="body2"
-								mt={2}>
-								This is a demo book for testing purposes
-							</Typography>
-						</Stack>
-					</CardContent>
-				</Card>
-				<Card>
-					<CardContent sx={{ display: 'flex', gap: '10px' }}>
-						<Paper
-							elevation={5}
-							sx={{
-								bgcolor: theme.palette.primary.main,
-								width: { xs: '50px' },
-								height: { xs: '75px' },
-							}}></Paper>
-						<Stack textAlign={'left'}>
-							<Typography variant="body1">Book Title</Typography>
-							<Typography
-								variant="body2"
-								color={'text.secondary'}>
-								by Book Author
-							</Typography>
-							<Typography
-								variant="body2"
-								mt={2}>
-								This is a demo book for testing purposes
-							</Typography>
-						</Stack>
-					</CardContent>
-				</Card>
+				{recentBooks}
 			</Stack>
-
 			<MaterialLink
 				component={Link}
 				to="/my-books">
