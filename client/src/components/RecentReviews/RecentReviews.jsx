075e725c72e46e54ce5a918dccdaed5afb717645
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-06-12 11:57:22 -0400

    feat: Update recent reviews to use data from the backend

--- client/src/components/RecentReviews/RecentReviews.jsx
+++ client/src/components/RecentReviews/RecentReviews.jsx
@@ -4,33 +4,36 @@ import CardHeader from '@mui/material/CardHeader';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import AccountCircle from '@mui/icons-material/AccountCircle';
 import Grid from '@mui/material/Grid';
-const userReviews = [
-	{
-		avatar: <AccountCircle></AccountCircle>,
-		name: 'Remy Sharp',
-		title: 'Book Title',
-		reviewTitle: 'Review Title',
-		review: "I absolutely love how versatile this product is! Whether I'm tackling work projects or indulging in my favorite hobbies, it seamlessly adapts to my changing needs. Its intuitive design has truly enhanced my daily routine, making tasks more efficient and enjoyable.",
-	},
-	{
-		avatar: <AccountCircle></AccountCircle>,
-		name: 'Travis Howard',
-		title: 'Book Title',
-		reviewTitle: 'Review Title',
-		review: "One of the standout features of this product is the exceptional customer support. In my experience, the team behind this product has been quick to respond and incredibly helpful. It's reassuring to know that they stand firmly behind their product.",
-	},
-	{
-		avatar: <AccountCircle></AccountCircle>,
-		name: 'Cindy Baker',
-		title: 'Book Title',
-		reviewTitle: 'Review Title',
-		review: 'The level of simplicity and user-friendliness in this product has significantly simplified my life. I appreciate the creators for delivering a solution that not only meets but exceeds user expectations.',
-	},
-];
+import { useSelector } from 'react-redux';
+import Avatar from '@mui/material/Avatar';
+import MaterialLink from '@mui/material/Link';
+import { Link } from 'react-router-dom';
 
 const RecentReviews = () => {
+	const books = useSelector(state => state.book.bookInfo);
+	let top3Books = [];
+
+	// Function to calculate average rating of books
+	function calculateAverageRating(reviews) {
+		const totalRating = reviews.reduce(
+			(acc, review) => acc + review.rating,
+			0
+		);
+		return totalRating / reviews.length;
+	}
+
+	if (books.length > 0) {
+		// Sort the books by average rating in descending order
+		const booksCopy = books.toSorted(
+			(a, b) =>
+				calculateAverageRating(b.reviews) -
+				calculateAverageRating(a.reviews)
+		);
+		// Get the top 3 books (highest average rating)
+		top3Books = booksCopy.slice(0, 3);
+	}
+
 	return (
 		<Box sx={{ backgroundColor: 'primary.main', color: 'secondary.main' }}>
 			<Container
@@ -57,65 +60,95 @@ const RecentReviews = () => {
 					<Typography
 						variant="body1"
 						sx={{ color: 'primary.main.400' }}>
-						See what users are saying about books you{"'"}ve
-						interacted with. Discover reviews that can offer
-						insightful analysis of a book{"'"}s strengths and
-						weaknesses, prompting you to consider aspects you might
-						have missed while reading.
+						See what users are saying about books you might like.
+						Book reviews can expose you to different perspectives on
+						a book{"'"}s plot, characters, and writing style,
+						helping you decide if it aligns with your reading
+						preferences. In short, they can be a compass to navigate
+						the vast sea of new releases and find books you{"'"}ll
+						truly enjoy.
 					</Typography>
 				</Box>
 				<Grid
 					container
 					spacing={2}>
-					{userReviews.map((review, index) => (
-						<Grid
-							item
-							xs={12}
-							sm={4}
-							key={index}
-							sx={{ display: 'flex' }}>
-							<Card
-								sx={{
-									display: 'flex',
-									flexDirection: 'column',
-									justifyContent: 'space-between',
-									border: '1px solid',
-									borderColor: 'secondary.main',
-									background: 'transparent',
-									backgroundColor: 'primary.main.500',
-									flexGrow: 1,
-									color: 'secondary.main',
-									p: 1,
-								}}>
-								<CardContent>
-									<Typography>
-										{review.reviewTitle}
-									</Typography>
-									<Typography variant="body2">
-										{review.review}
-									</Typography>
-								</CardContent>
-								<Box
+					{top3Books.length > 0 ? (
+						top3Books.map(book => (
+							<Grid
+								item
+								xs={12}
+								md={4}
+								key={book.reviews[0]._id}
+								sx={{ display: 'flex' }}>
+								<Card
 									sx={{
 										display: 'flex',
-										flexDirection: 'row',
+										flexDirection: 'column',
 										justifyContent: 'space-between',
-										pr: 2,
+										border: '1px solid',
+										borderColor: 'secondary.main',
+										background: 'transparent',
+										backgroundColor: 'primary.main.500',
+										flexGrow: 1,
+										color: 'secondary.main',
+										p: 1,
 									}}>
-									<CardHeader
-										avatar={review.avatar}
-										title={review.name}
-										subheader={review.title}
-										subheaderTypographyProps={{
-											color: 'inherit',
-											fontWeight: 'bold',
-											textAlign: 'left',
-										}}
-									/>
-								</Box>
-							</Card>
-						</Grid>
-					))}
+									<CardContent>
+										<Typography
+											variant="h6"
+											mb={2}>
+											{book.reviews[0].title}
+										</Typography>
+										<Typography variant="body2">
+											{book.reviews[0].body}
+										</Typography>
+									</CardContent>
+									<Box
+										sx={{
+											display: 'flex',
+											flexDirection: 'row',
+											justifyContent: 'space-between',
+											pr: 2,
+										}}>
+										<CardHeader
+											avatar={
+												<Avatar
+													src={
+														book.reviews[0].userId
+															.image
+													}></Avatar>
+											}
+											title={book.reviews[0].userId.name}
+											subheader={
+												<MaterialLink
+													component={Link}
+													to={'/'}
+													color={'#ffffff'}>
+													{book.title}
+												</MaterialLink>
+											}
+											subheaderTypographyProps={{
+												color: 'inherit',
+												fontWeight: 'bold',
+												textAlign: 'left',
+											}}
+											titleTypographyProps={{
+												textAlign: 'left',
+											}}
+										/>
+									</Box>
+								</Card>
+							</Grid>
+						))
+					) : (
+						<Typography
+							variant="h6"
+							width={'100%'}
+							mt={4}>
+							No Reviews to display at this time. Please try again
+							later...
+						</Typography>
+					)}
 				</Grid>
 			</Container>
 		</Box>
