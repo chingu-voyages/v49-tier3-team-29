c2608fa681d2c6104e0af19b0251c13773d8a2de
Alexander Hunt <alexhunt215@gmail.com>
2024-06-12 23:50:05 -0700

    fixed: merge conflict for 'fetchList' thunk function update to invoke the api call by username

--- client/src/reducers/listSlice.js
+++ client/src/reducers/listSlice.js
@@ -9,13 +9,8 @@ const initialState = {
 };
 
 // * Fetch list
-<<<<<<< feat/import-recent-books-data
 export const fetchList = createAsyncThunk('list/', async username => {
 	const request = await axios.get(`${listBaseURL}/user/${username}`);
-=======
-export const fetchList = createAsyncThunk('list/', async () => {
-	const request = await axios.get(`${listBaseURL}/`);
->>>>>>> dev
 	const response = await request.data;
 	return response;
 });
@@ -40,10 +35,6 @@ const listSlice = createSlice({
 				(state, action) => {
 					state.loading = false;
 					state.listInfo = action.payload;
-<<<<<<< feat/import-recent-books-data
-=======
-					state.error = '';
->>>>>>> dev
 				}
 			)
 			.addMatcher(
