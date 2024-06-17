2c6166fdf6bd553c627b9bd6e8beae1aa74b0f84
bank1e <cub0sextra@gmail.com>
2024-06-16 22:58:36 -0700

    Feat/logout-frontend
    [backend]
    - Added POST route to log out user.
    - Added POST function to log out user.
    [frontend]
    - Added handleLogout function in Navbar message user and dispatch logoutUser function to store.
    - Added Thunk to userSlice to log out user.

--- client/src/reducers/userSlice.js
+++ client/src/reducers/userSlice.js
@@ -1,111 +1,108 @@
-import axios from 'axios';
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { userBaseURL } from '../utils/baseUrl';
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { userBaseURL } from "../utils/baseUrl";
 
 const initialState = {
-	loading: false,
-	userInfo: {},
-	error: '',
+  loading: false,
+  userInfo: {},
+  error: "",
 };
 
 // * Login User
 export const loginUser = createAsyncThunk(
-	'user/loginUser',
-	async userCredentials => {
-		const request = await axios.post(
-			`${userBaseURL}/login`,
-			userCredentials
-		);
-		const response = await request.data;
+  "user/loginUser",
+  async (userCredentials) => {
+    const request = await axios.post(`${userBaseURL}/login`, userCredentials);
+    const response = await request.data;
 
-		return response;
-	}
+    return response;
+  }
 );
 
 // * Logout User
-export const logoutUser = createAsyncThunk('user/logoutUser', async () => {});
-
+export const logoutUser = createAsyncThunk("user/logoutUser", async () => {
+  const request = await axios.post(`${userBaseURL}/logout`);
+  const response = await request.data;
+  return response;
+});
 // * Register User
 export const registerUser = createAsyncThunk(
-	'user/registerUser',
-	async userDetails => {
-		const request = await axios.post(
-			`${userBaseURL}/register`,
-			userDetails
-		);
-		const response = await request.data;
+  "user/registerUser",
+  async (userDetails) => {
+    const request = await axios.post(`${userBaseURL}/register`, userDetails);
+    const response = await request.data;
 
-		return response;
-	}
+    return response;
+  }
 );
 
 // * Authenticate/ restore logged In user
 export const authenticateUser = createAsyncThunk(
-	'user/authentication',
-	async userId => {
-		const request = await axios.get(`${userBaseURL}/${userId}`);
-		const response = await request.data;
-		return response;
-	}
+  "user/authentication",
+  async (userId) => {
+    const request = await axios.get(`${userBaseURL}/${userId}`);
+    const response = await request.data;
+    return response;
+  }
 );
 
 const userSlice = createSlice({
-	name: 'user',
-	initialState,
-	extraReducers: builder => {
-		builder
-			.addMatcher(
-				action => {
-					return (
-						action.type === loginUser.pending.type ||
-						action.type === logoutUser.pending.type ||
-						action.type === authenticateUser.pending.type ||
-						action.type === registerUser.pending.type
-					);
-				},
-				state => {
-					state.loading = true;
-				}
-			)
-			.addMatcher(
-				action => {
-					return (
-						action.type === loginUser.fulfilled.type ||
-						action.type === authenticateUser.fulfilled.type ||
-						action.type === registerUser.fulfilled.type
-					);
-				},
-				(state, action) => {
-					state.loading = false;
-					state.userInfo = action.payload;
-					state.error = '';
-				}
-			)
-			.addMatcher(
-				action => action.type === logoutUser.fulfilled.type,
-				state => {
-					state.loading = false;
-					state.userInfo = {};
-					state.error = '';
-				}
-			)
-			.addMatcher(
-				action => {
-					return (
-						action.type === loginUser.rejected.type ||
-						action.type === logoutUser.rejected.type ||
-						action.type === authenticateUser.rejected.type ||
-						action.type === registerUser.rejected.type
-					);
-				},
-				(state, action) => {
-					state.loading = false;
-					state.userInfo = {};
-					state.error = action.error.message;
-					console.log(action.error.message);
-				}
-			);
-	},
+  name: "user",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(
+        (action) => {
+          return (
+            action.type === loginUser.pending.type ||
+            action.type === logoutUser.pending.type ||
+            action.type === authenticateUser.pending.type ||
+            action.type === registerUser.pending.type
+          );
+        },
+        (state) => {
+          state.loading = true;
+        }
+      )
+      .addMatcher(
+        (action) => {
+          return (
+            action.type === loginUser.fulfilled.type ||
+            action.type === authenticateUser.fulfilled.type ||
+            action.type === registerUser.fulfilled.type
+          );
+        },
+        (state, action) => {
+          state.loading = false;
+          state.userInfo = action.payload;
+          state.error = "";
+        }
+      )
+      .addMatcher(
+        (action) => action.type === logoutUser.fulfilled.type,
+        (state) => {
+          state.loading = false;
+          state.userInfo = {};
+          state.error = "";
+        }
+      )
+      .addMatcher(
+        (action) => {
+          return (
+            action.type === loginUser.rejected.type ||
+            action.type === logoutUser.rejected.type ||
+            action.type === authenticateUser.rejected.type ||
+            action.type === registerUser.rejected.type
+          );
+        },
+        (state, action) => {
+          state.loading = false;
+          state.userInfo = {};
+          state.error = action.error.message;
+          console.log(action.error.message);
+        }
+      );
+  },
 });
 
 export default userSlice.reducer;
